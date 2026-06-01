31934b22452da04fba9f096b13576faec81d8fb6
abiwuamk17-cmd <abiwuamk17@gmail.com>
2026-06-01 20:11:52 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Micheal Kofi Abiwu
+# STUDENT ID:  2425402790
+# Email: 2425402790@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Micheal Kofi Abiwu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials MKA in small text, then in large stylized block letters.
 
+    print()
+    print("...MKA")
+    print()
+    print("...MM........MM........KK.....KKKKK............AAAA.........")
+    print("...MMM......MMM........KK....KKKK.............AAAAAA........")
+    print("...MMMM....MMMM........KK...KKK..............AA....AA.......")
+    print("...MM.MM..MM.MM........KK..KKK..............AA......AA......")
+    print("...MM..MMMM..MM........KKKKKKK..............AA......AA......")
+    print("...MM...MM...MM........KKKKKKK..............AAAAAAAAAA......")
+    print("...MM........MM........KK..KKK.............AA........AA.....")
+    print("...MM........MM........KK...KKK............AA........AA.....")
+    print("...MM........MM...**...KK....KKKK.....**...AA........AA...**")
+    print("...MM........MM...**...KK.....KKKKK...**...AA........AA...**")
+    print()
 
 
 main()
